845ce5c97558fa1c31608ff2c80669e7d587bac7
Adejoke <adejokeogunyinka@enyata.com>
2025-03-22 20:17:07 +0100

    feat: dashboard home page conversion

---
+++ app/home/page.tsx
@@ -0,0 +1,81 @@
+import { IconTriangleAlert } from "@/components/icons";
+import {
+  BookAppointmentCard,
+  DashboardStatCard,
+  FamilyAndFriendsCard,
+  UpcomingAppointmentCard,
+  WalletBalanceCard,
+  ProfileCard,
+  FavouriteProvidersCard,
+  NotificationsCard,
+} from "@/components/shared";
+import { Button } from "@/components/ui";
+
+const Dashboard = () => {
+  const dashboardStats = [
+    { id: 1, title: "Completed Appointments", value: 53 },
+    { id: 2, title: "Upcoming Appointments", value: 53 },
+    { id: 3, title: "Average Booking/Week", value: 3 },
+  ];
+  return (
+    <div className="w-full grid gap-y-4 md:gap-y-8">
+      <div className="border border-status-danger p-3 md:p-6 bg-status-light-red rounded-lg w-full">
+        <div className="flex gap-3 items-start md:items-center flex-col md:flex-row ">
+          <div className="flex gap-2 md:gap-3">
+            <div className="md:pt-1">
+              <IconTriangleAlert className="stroke-status-danger size-4" />
+            </div>
+
+            <div className="grid gap-y-1">
+              <h2 className="text-status-danger font-semibold text-sm md:text-lg">
+                Complete Your Profile for Full Access
+              </h2>
+              <p className="text-text-2 text-xs md:text-sm">
+                A complete profile helps you connect faster, build trust, and
+                access all platform features—whether you're booking sessions,
+                offering consultations, or managing clients. Update your details
+                in just a few minutes!
+              </p>
+            </div>
+          </div>
+
+          <div className="ml-6 md:ml-0">
+            <Button className="bg-button-primary h-[38px] rounded-[100px]">
+              Complete Profile
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        {dashboardStats.map((stat, index) => (
+          <div
+            key={stat.id}
+            className={index === 0 ? "col-span-2 md:col-span-1" : "col-span-1"}
+          >
+            <DashboardStatCard
+              title={stat.title}
+              value={stat.value}
+              index={index}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-5 items-center">
+        <BookAppointmentCard />
+        <UpcomingAppointmentCard />
+        <WalletBalanceCard />
+        <FamilyAndFriendsCard />
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-[21px] items-center">
+        <ProfileCard />
+        <FavouriteProvidersCard />
+        <NotificationsCard />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
