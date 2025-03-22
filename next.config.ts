0a654081882c338ea50fdaa5bca2e179fb2080a2
Adejoke <adejokeogunyinka@enyata.com>
2025-03-22 20:10:56 +0100

    feat: dashboard layout

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	experimental: {
-		viewTransition: true,
+  experimental: {
+    viewTransition: true,
   },
   compiler: {
-    removeConsole: process.env.NODE_ENV === "production"
+    removeConsole: process.env.NODE_ENV === "production",
   },
-	// basePath: '/home',
-	// async redirects() {
-	// 	return [
-	// 		{
-	// 			source: '/',
-	// 			destination: '/home',
-	// 			basePath: false,
-	// 			permanent: false
-	// 		}
-	// 	]
-	// },
-	reactStrictMode: true
+  images: {
+    remotePatterns: [{ hostname: "plus.unsplash.com" }],
+  },
+  // basePath: '/home',
+  // async redirects() {
+  // 	return [
+  // 		{
+  // 			source: '/',
+  // 			destination: '/home',
+  // 			basePath: false,
+  // 			permanent: false
+  // 		}
+  // 	]
+  // },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
