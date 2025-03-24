2176e835c981e11c97819975e4488247e2b8d3ca
Adejoke <adejokeogunyinka@enyata.com>
2025-03-24 22:33:21 +0100

    fix: dashboard home page mobile responsiveness

--- components/shared/dashboard/wallet-balance-card.tsx
+++ components/shared/dashboard/wallet-balance-card.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui";
 
 export const WalletBalanceCard = () => {
   return (
-    <div className="relative p-6 w-full md:w-[48%] xl:w-[32%] 3xl:w-[283px] max-w-full md:max-w-[48%] xl:max-w-[32%] 3xl:max-w-[283px] h-[370px] rounded-2xl bg-text-bg-3 grid gap-y-1 content-start">
+    <div className="order-3 relative p-6 w-full md:w-[48%] xl:w-[32%] 3xl:!w-[283px] max-w-full md:max-w-[48%] xl:max-w-[32%] 3xl:!max-w-[283px] h-[370px] rounded-2xl bg-text-bg-3 grid gap-y-1 content-start">
       <h5 className="text-sm text-white">Wallet balance</h5>
       <p className="text-2xl text-white leading-[140%]">₦235,402,853</p>
       <Button className="bg-white text-button-primary w-fit rounded-[100px] mt-2 font-semibold">
