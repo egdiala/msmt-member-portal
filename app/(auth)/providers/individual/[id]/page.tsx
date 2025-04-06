1ddc37033e9158bc3116be98a44aa93317076de1
Adejoke <adejokeogunyinka@enyata.com>
2025-04-06 22:08:22 +0100

    feat: complete single individual provider page

--- app/(auth)/providers/individual/[id]/page.tsx
+++ app/(auth)/providers/individual/[id]/page.tsx
@@ -1,5 +1,10 @@
-import { IconPlus, IconStarFull } from "@/components/icons";
-import { BreadcrumbCmp } from "@/components/shared";
+import {
+  IconAudioLines,
+  IconPlus,
+  IconStarFull,
+  IconVideo,
+} from "@/components/icons";
+import { BreadcrumbCmp, RenderIf } from "@/components/shared";
 import { Avatar, AvatarImage, Button } from "@/components/ui";
 
 const SingleIndividualProvider = () => {
@@ -22,6 +27,23 @@ const SingleIndividualProvider = () => {
     },
   ];
 
+  const services = [
+    "Couple counselling",
+    "Family counselling",
+    "Child/Adolescent counselling",
+    "Gambling addiction",
+    "Sex counselling",
+    "Psychological evaluation",
+    "Drug & Alcohol treatment",
+    "Adult individual counselling",
+  ];
+
+  const cardStats = [
+    { id: 1, title: "Completed appointment", value: "152" },
+    { id: 2, title: "Charge", value: "45" },
+    { id: 3, title: "Communication preferences", value: ["Video", "Audio"] },
+  ];
+
   return (
     <div className="grid gap-y-2">
       <BreadcrumbCmp
@@ -31,9 +53,9 @@ const SingleIndividualProvider = () => {
         ]}
       />
 
-      <div className="bg-white rounded-2xl p-6 gap-y-5">
-        <div className="rounded-lg bg-blue-400 p-3 flex gap-x-3">
-          <Avatar className="w-39 h-39 rounded-sm">
+      <div className="bg-white rounded-2xl p-3 md:p-6 grid gap-y-5">
+        <div className="rounded-lg bg-blue-400 p-3 flex gap-3 flex-col md:flex-row">
+          <Avatar className="w-full md:w-39 h-39 rounded-sm">
             <AvatarImage
               className="object-cover"
               src="https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -58,7 +80,7 @@ const SingleIndividualProvider = () => {
                 Mark as Favourite
               </Button>
 
-              <Button>
+              <Button className="hidden md:inline-flex">
                 <IconPlus className="stroke-white" />
                 Book An Appointment
               </Button>
@@ -74,7 +96,72 @@ const SingleIndividualProvider = () => {
             </div>
           ))}
         </div>
-        <div></div>
+
+        <div className="border border-divider rounded-lg px-5 py-4 grid gap-y-2">
+          <p className="text-brand-2 text-sm">Services</p>
+
+          <div className="flex gap-4 flex-wrap">
+            {services.map((service) => (
+              <div
+                key={service}
+                className="rounded-full bg-grey-400 px-4 py-2.5"
+              >
+                {service}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+          {cardStats.map((stat) => (
+            <div
+              key={stat.id}
+              className="grid gap-2 border border-divider rounded-lg px-5 py-4"
+            >
+              <p className="text-brand-2 text-sm">{stat.title}</p>
+              <RenderIf condition={stat.title !== "Communication preferences"}>
+                <p className="text-lg text-brand-1 font-medium">{stat.value}</p>
+              </RenderIf>
+
+              <div className="flex items-center gap-x-2">
+                <RenderIf
+                  condition={
+                    stat.title === "Communication preferences" &&
+                    stat.value?.includes("Video")
+                  }
+                >
+                  <Button
+                    variant="outline"
+                    className="border-button-primary bg-blue-400 w-fit text-button-primary hover:text-button-primary"
+                  >
+                    <IconVideo className="stroke-button-primary" />
+                    Video
+                  </Button>
+                </RenderIf>
+
+                <RenderIf
+                  condition={
+                    stat.title === "Communication preferences" &&
+                    stat.value?.includes("Audio")
+                  }
+                >
+                  <Button
+                    variant="outline"
+                    className="border-button-primary bg-blue-400 w-fit text-button-primary hover:text-button-primary"
+                  >
+                    <IconAudioLines className="stroke-button-primary" />
+                    Audio
+                  </Button>
+                </RenderIf>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <Button className="flex md:hidden">
+          <IconPlus className="stroke-white" />
+          Book An Appointment
+        </Button>
       </div>
     </div>
   );
