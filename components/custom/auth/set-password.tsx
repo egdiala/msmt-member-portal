4b1efcfe10aaeb65b26c4a8f464ed623104692d4
Victoria <reubenvickie20@gmail.com>
2025-04-12 18:14:39 +0100

    feat: add password input component and validation schema

--- components/custom/auth/set-password.tsx
+++ components/custom/auth/set-password.tsx
@@ -5,20 +5,12 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import PasswordInput from "@/components/shared/password-input";
 import type * as z from "zod";
 import { AnimatePresence, motion } from "motion/react";
 import useMeasure from "react-use-measure";
-
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
-import { FloatingInput } from "@/components/shared/floating-input";
-import { IconEye, IconEyeOff } from "@/components/icons";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { setNewPasswordSchema } from "@/lib/validations";
 import { useResetPassword } from "@/services/hooks/mutations/use-auth";
 import { Loader } from "@/components/shared/loader";
@@ -26,9 +18,6 @@ import { Loader } from "@/components/shared/loader";
 export default function SetNewPassword() {
   const router = useRouter();
   const [ref, bounds] = useMeasure();
-
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const { mutate, isPending } = useResetPassword((href) => {
     localStorage.removeItem("email-for-reset");
@@ -87,33 +76,14 @@ export default function SetNewPassword() {
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <div className="relative">
-                        <FloatingInput
-                          label="Password"
-                          type={showPassword ? "text" : "password"}
-                          autoComplete="current-password"
-                          className=" pr-10"
-                          {...field}
-                        />
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          size="icon"
-                          className="absolute right-0 inset-y-1 h-10 w-10"
-                          onClick={() => setShowPassword(!showPassword)}
-                        >
-                          {showPassword ? (
-                            <IconEyeOff className="h-4 w-4  stroke-brand-3 " />
-                          ) : (
-                            <IconEye className="h-4 w-4 stroke-brand-3 " />
-                          )}
-                          <span className="sr-only">
-                            {showPassword ? "Hide password" : "Show password"}
-                          </span>
-                        </Button>
-                      </div>
+                      <PasswordInput
+                        value={field.value}
+                        onChange={field.onChange}
+                        id={field.name}
+                        name={field.name}
+                        labelTitle="Password"
+                      />
                     </FormControl>
-                    <FormMessage />
                   </FormItem>
                 )}
               />
@@ -123,37 +93,14 @@ export default function SetNewPassword() {
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <div className="relative">
-                        <FloatingInput
-                          label="Confirm Password"
-                          type={showConfirmPassword ? "text" : "password"}
-                          autoComplete="current-password"
-                          className=" pr-10"
-                          {...field}
-                        />
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          size="icon"
-                          className="absolute right-0 inset-y-1 h-10 w-10"
-                          onClick={() =>
-                            setShowConfirmPassword(!showConfirmPassword)
-                          }
-                        >
-                          {showConfirmPassword ? (
-                            <IconEyeOff className="h-4 w-4  stroke-brand-3 " />
-                          ) : (
-                            <IconEye className="h-4 w-4 stroke-brand-3 " />
-                          )}
-                          <span className="sr-only">
-                            {showConfirmPassword
-                              ? "Hide password"
-                              : "Show password"}
-                          </span>
-                        </Button>
-                      </div>
+                      <PasswordInput
+                        value={field.value}
+                        onChange={field.onChange}
+                        id={field.name}
+                        labelTitle="Confirm Password"
+                        name={field.name}
+                      />
                     </FormControl>
-                    <FormMessage />
                   </FormItem>
                 )}
               />
