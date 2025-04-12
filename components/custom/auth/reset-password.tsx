b74253d008d4a727aff84103cc191bf1bd03f312
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2025-04-12 20:48:53 +0100

    chore: seo and stuff

--- components/custom/auth/reset-password.tsx
+++ components/custom/auth/reset-password.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useMemo } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -54,77 +53,55 @@ export default function ResetPassword() {
   }, [isPending]);
 
   return (
-    <div className="space-y-6 max-w-[650px] mx-auto w-full px-2">
-      <div className="space-y-1 text-center">
-        <h1 className="lg:text-2xl text-brand-1 font-bold">Enter Your Email</h1>
-        <p className="text-sm text-brand-2">
-          Enter the email associated with your account. We would send password
-          reset instructions to your email
-        </p>
-      </div>
-
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <motion.div animate={{ height: bounds.height }}>
-            <div
-              ref={ref}
-              className="bg-white w-full rounded-xl p-4 lg:p-6 space-y-4"
-            >
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <div className="relative">
-                        <FloatingInput
-                          label="Email"
-                          type="email"
-                          autoComplete="email"
-                          className=" pr-10"
-                          {...field}
-                        />
-                        <div className="absolute right-3 top-1/2 -translate-y-1/2 stroke-brand-3 pointer-events-none">
-                          <IconEmail className="h-4 w-4" />
-                        </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <motion.div animate={{ height: bounds.height }}>
+          <div
+            ref={ref}
+            className="bg-white w-full rounded-xl p-4 lg:p-6 space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="relative">
+                      <FloatingInput
+                        label="Email"
+                        type="email"
+                        autoComplete="email"
+                        className=" pr-10"
+                        {...field}
+                      />
+                      <div className="absolute right-3 top-1/2 -translate-y-1/2 stroke-brand-3 pointer-events-none">
+                        <IconEmail className="h-4 w-4" />
                       </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </motion.div>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </motion.div>
 
-          <motion.div layout className="flex items-center justify-center">
-            <Button type="submit" disabled={isPending} className="w-41">
-              <AnimatePresence mode="popLayout" initial={false}>
-                <motion.span
-                  transition={{ type: "spring", duration: 0.3, bounce: 0 }}
-                  initial={{ opacity: 0, y: -25 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 25 }}
-                  key={buttonState}
-                >
-                  {buttonCopy[buttonState]}
-                </motion.span>
-              </AnimatePresence>
-            </Button>
-          </motion.div>
-        </form>
-      </Form>
-
-      <div className="text-center text-sm font-semibold">
-        <span className="text-brand-btn-secondary">
-          Already have an account {""}
-        </span>
-        <Link
-          href="/sign-in"
-          className="text-brand-accent-2 underline transition-colors  hover:opacity-80 "
-        >
-          Sign in Instead
-        </Link>
-      </div>
-    </div>
+        <motion.div layout className="flex items-center justify-center">
+          <Button type="submit" disabled={isPending} className="w-41">
+            <AnimatePresence mode="popLayout" initial={false}>
+              <motion.span
+                transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+                initial={{ opacity: 0, y: -25 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 25 }}
+                key={buttonState}
+              >
+                {buttonCopy[buttonState]}
+              </motion.span>
+            </AnimatePresence>
+          </Button>
+        </motion.div>
+      </form>
+    </Form>
   );
 }
--- components/custom/auth/set-password.tsx
+++ components/custom/auth/set-password.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -21,6 +20,7 @@ export default function SetNewPassword() {
 
   const { mutate, isPending } = useResetPassword((href) => {
     localStorage.removeItem("email-for-reset");
+    localStorage.removeItem("otp-for-reset");
     router.push(href);
   });
 
@@ -56,13 +56,6 @@ export default function SetNewPassword() {
   }, [isPending]);
 
   return (
-    <div className="space-y-6 max-w-[650px] mx-auto w-full px-2">
-      <div className="space-y-1 text-center">
-        <h1 className="lg:text-2xl font-bold">Set New Password</h1>
-        <p className="text-sm text-brand-2">
-          Create a new password to keep your account secure.
-        </p>
-      </div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <motion.div animate={{ height: bounds.height }}>
@@ -125,17 +118,5 @@ export default function SetNewPassword() {
           </motion.div>
         </form>
       </Form>
-      <div className="text-center text-sm font-semibold">
-        <span className="text-brand-btn-secondary">
-          Do not have an account?{" "}
-        </span>
-        <Link
-          href="/sign-up"
-          className="text-brand-accent-2 underline transition-colors  hover:opacity-80 "
-        >
-          Sign up instead
-        </Link>
-      </div>
-    </div>
   );
 }
