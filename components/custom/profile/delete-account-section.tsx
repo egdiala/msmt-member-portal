e15c15c408597e3f148c68e0b19e3ebef7a17dcd
Adejoke <adejokeogunyinka@enyata.com>
2025-05-14 14:39:32 +0100

    fix: build error fixes

--- components/custom/profile/delete-account-section.tsx
+++ components/custom/profile/delete-account-section.tsx
@@ -14,8 +14,8 @@ export const DeleteAccountSection = () => {
           Delete Account
         </h2>
         <p className="text-xs md:text-sm text-text-2">
-          This will permanently remove your details from MSMT. You'll need to
-          sign up again to use the platform.
+          {`This will permanently remove your details from MSMT. You'll need to
+          sign up again to use the platform.`}
         </p>
       </div>
 
